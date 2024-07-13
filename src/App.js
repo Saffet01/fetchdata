bfce7b61165b1e196f9ed08205ca2fa75fce76ae
Saffet01 <saffetr.kaban@gmail.com>
2024-07-13 22:21:58 +0300

    feat: Post detail with popup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Post } from "./components/Post";
 function App() {
   const [posts, setPosts] = useState([]);
   const [newPost, setNewPost] = useState({title: "", body: ""});
+  const [selectedPost, setSelectedPost] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -15,9 +16,9 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const postId = {...newPost, id: posts.length + 1};
+    const postId = {...newPost, id: posts.length + 1 };
+    setNewPost({title: "", body: ""});
     setPosts([postId, ...posts]);
-    setNewPost({title: "", body: ""})
   }
 
   const handleInputChange = (event) => {
@@ -29,8 +30,13 @@ function App() {
     setPosts(posts.filter((post) => post.id !== id));
   }
 
+  const handlePostClick = (post) => {
+    setSelectedPost(post);
+  };
+
   return (
     <div className="App">
+      
       <h1>Blog Posts</h1>
       <form onSubmit={handleSubmit}>
         <input className="input-field" name="title" type="text" value={newPost.title} onChange={handleInputChange} placeholder="Title" required/>
@@ -39,9 +45,18 @@ function App() {
       </form>
       <div className="posts-container">
         {posts.map((post) => (
-          <Post key={post.id} post={post} onDelete={handleDeletePost} />
+          <Post key={post.id} post={post} onClick={handlePostClick} onDelete={handleDeletePost} />
         ))}
       </div>
+      {selectedPost && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setSelectedPost(null)}>&times;</span>
+            <h2>{selectedPost.title}</h2>
+            <p>{selectedPost.body}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Post.css";
 
-export const Post = ({ post, onDelete }) => {
+export const Post = ({ post, onDelete, onClick }) => {
   return (
-    <div className="post-card">
+    <div className="post-card" onClick={() => onClick(post)}>
       <div className="post-content">
         <h2>{post.title}</h2>
         <p>{post.body}</p>
